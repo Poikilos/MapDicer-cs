116dd2278c64cc21073ac5786bb0bcb9047379f8
Jake Gustafson <7557867+poikilos@users.noreply.github.com>
2020-12-07 02:41:10 -0500

    Add main UI.

--- MapDicer/MainPage.xaml.cs
+++ MapDicer/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,136 @@ namespace MapDicer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private int touchSize = 16;
+        public static MainPage thisMP = null;
+        private const string newItemContent = "(Add New)";
+        private void afterBrushSize()
+        {
+            this.menusSP.Width = this.menusBarSP.Width - this.brushColorShape.Width;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // See https://stackoverflow.com/a/34832454 
+            // CC BY-SA 3.0 Vadim Martynov
+            try
+            {
+                this.brushTerrainCB.Items.Add(e.Parameter as Terrain);
+            }
+            catch (System.ArgumentException ex)
+            {
+                // The system gave us the page, not the add terrain page.
+            }
+        }
+        private void afterSize(bool InitializeBrush)
+        {
+            
+            this.touchSize = Math.Max((int)this.Height / 800, 1) * 16;
+            this.mainSP.Width = this.Width;
+            this.menusBarSP.Width = this.mainSP.Width;
+            this.menusBarSP.Height = touchSize * 4;
+            this.brushParamSlider.Minimum = 0;
+            this.brushParamSlider.Maximum = 65535;
+            this.brushSizeSlider.Minimum = 1;
+            this.brushSizeSlider.Maximum = touchSize * 4;
+            if (InitializeBrush) {
+                this.brushColorShape.Width = touchSize * 2;
+            }
+            // Always set H since W changes elsewhere:
+            this.brushColorShape.Height = this.brushColorShape.Width;
+            double slackW = this.menusBarSP.Width - this.brushColorShape.Width;
+            this.navSP.Width = slackW;
+            this.navSP.Height = this.touchSize * 2;
+            this.selectSP.Width = slackW;
+            this.selectSP.Height = this.touchSize * 2; // detail, region, block, settings
+            this.settingsButton.Width = this.touchSize * 6;
+            double selectW = this.selectSP.Width;
+            double selectSlackW = selectW - this.settingsButton.Width;
+            this.detailCB.Width = selectSlackW * .33;
+            this.detailCB.Height = this.settingsButton.Height;
+            this.regionCB.Width = selectSlackW * .34;
+            this.regionCB.Height = this.detailCB.Height;
+            this.blockCB.Width = selectSlackW * .33;
+            this.blockCB.Height = this.detailCB.Height;
+            this.settingsButton.Height = this.touchSize * 2;
+            this.settingsButton.FontSize = touchSize;
+            this.brushSizeImage.Width = this.touchSize * 2;
+            this.brushSizeImage.Height = this.brushSizeImage.Width;
+            this.brushParamImage.Width = this.brushSizeImage.Width;
+            this.brushParamImage.Height = this.brushParamImage.Width;
+            double slackWithoutTwoW = slackW - this.brushSizeImage.Width - this.brushParamImage.Width;
+            this.brushTerrainCB.Width = Math.Round((double)slackWithoutTwoW * .34);
+            this.brushTerrainCB.Height = touchSize * 2;
+            this.brushParamSlider.Width = Math.Round((double)slackWithoutTwoW * .33);
+            this.brushParamSlider.Height = this.brushTerrainCB.Height;
+            this.brushSizeSlider.Width = Math.Round((double)slackWithoutTwoW * .33);
+            this.brushSizeSlider.Height = this.brushTerrainCB.Height;
+            if (InitializeBrush) { 
+                this.brushSizeSlider.Value = this.brushColorShape.Width;
+            }
+            this.afterBrushSize();
+        }
         public MainPage()
         {
             this.InitializeComponent();
+            this.afterSize(true);
+            MainPage.thisMP = this;
+            Terrain newTerrain = new Terrain(newItemContent, 0, 0, 0);
+            newTerrain.Click += NewTerrain_Click;
+            this.brushTerrainCB.Items.Add(newTerrain);
+            // Button: MessageDialog dialog = new MessageDialog((this.brushTerrainCB.Items[0]).GetType().ToString());
+            // var result = dialog.ShowAsync();
+
+            //this.brushTerrainCB.Items.Add(typeof this.brushTerrainCB.Items[0]);
+
+        }
+
+        private void NewTerrain_Click(object sender, RoutedEventArgs e)
+        {
+            goToAddTerrain();
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.afterSize(true);
+        }
+
+        private void brushSizeSlider_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+        }
+
+        private void brushSizeSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            this.brushColorShape.Width = this.brushSizeSlider.Value;
+            this.afterSize(false);
+
+        }
+
+        internal static void AddTerrain(string name, double r, double g, double b)
+        {
+            thisMP.brushColorShape.Fill = new SolidColorBrush(Windows.UI.Color.FromArgb(255, (byte)r, (byte)g, (byte)b));
+            thisMP.brushTerrainCB.Items.Add(new Terrain("name", r, g, b));
+        }
+
+        private void settingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(SettingsPage), null);
+        }
+
+        private void brushTerrainCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((this.brushTerrainCB.SelectedItem != null) && (((Terrain)this.brushTerrainCB.SelectedItem).Content.Equals(newItemContent)))
+            {
+                goToAddTerrain();
+            }
+        }
+
+        private void goToAddTerrain()
+        {
+            // this.Frame.Navigate(typeof(NewTerrainPage), null);
+            // ^ Don't do that, it generates a new page.
+            // this.mainPage.NavigationService.Navigate(new Uri("/Views/Page.xaml?parameter=test", UriKind.Relative));
+            // ^ CC BY-SA 3.0 https://creativecommons.org/licenses/by-sa/3.0/ Daniel Little on https://stackoverflow.com/a/12444817/4541104
         }
     }
 }
---
+++ MapDicer/Terrain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapDicer
+{
+    /// <summary>
+    /// It must be a button to be a combo box Item.
+    /// </summary>
+    class Terrain : Windows.UI.Xaml.Controls.Button
+    {
+        public byte R;
+        public byte G;
+        public byte B;
+        // inherits Text from Button
+        public Terrain(string Content, double R, double G, double B)
+        {
+            this.R = (byte)R;
+            this.G = (byte)G;
+            this.B = (byte)B;
+            this.Content = Content;
+        }
+    }
+}
